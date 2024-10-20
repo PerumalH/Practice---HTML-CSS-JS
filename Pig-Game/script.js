01fa4f74d1e6c7997f0bef711cf524245f488dd4
PerumalH <perumalhpk250@gmail.com>
2024-10-20 23:05:58 +0530

    Start : Pig Game

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,22 +1,22 @@
-const secureBooking = function () {
-  let passCount = 0;
+// const secureBooking = function () {
+//   let passCount = 0;
 
-  return function () {
-    passCount++;
-    console.log(passCount);
-  };
-};
+//   return function () {
+//     passCount++;
+//     console.log(passCount);
+//   };
+// };
 
-const booker = secureBooking();
-booker();
+// const booker = secureBooking();
+// booker();
 
-(function () {
-  const header = document.querySelector("h1");
-  header.style.color = "blue";
-  let i = 0;
-  document.body.addEventListener("click", function () {
-    if (i % 2 == 0) header.style.color = "red";
-    else header.style.color = "blue";
-    i++;
-  });
-})();
+// (function () {
+//   const header = document.querySelector("h1");
+//   header.style.color = "blue";
+//   let i = 0;
+//   document.body.addEventListener("click", function () {
+//     if (i % 2 == 0) header.style.color = "red";
+//     else header.style.color = "blue";
+//     i++;
+//   });
+// })();
